8634b486e7a49f2b42864ca3d006e4a3ddfb7ab6
ivanft747 <ivanft747@bk.ru>
2025-05-14 21:28:57 +0300

    Добавьте файлы проекта.

---
+++ HomeworkArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace HomeworkArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Сколько элементов в массиве вы хотите ? ");
+            Console.WriteLine();
+            int size = int.Parse(Console.ReadLine());
+            int[] number = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("Введите число для элемента " + i + " : ");
+                number[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Вы ввели массив : ");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(number[i] + " ");
+            }
+        }
+    }
+}
