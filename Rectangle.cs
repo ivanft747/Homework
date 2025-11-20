b420ee54da2b4e5e33b0d2c62fd8c8db26c12bff
ivanft747 <ivanft747@bk.ru>
2025-11-21 00:17:45 +0300

    Add files via upload

---
+++ Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml;
+namespace HomeworkRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите высоту прямоугольника");
+                int height = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите длинну прямоугольника");
+                int width = int.Parse(Console.ReadLine());
+                Console.WriteLine("");
+                for (int i = 0;i < height; i++)
+                {
+                    for (int j = 0;j < width; j++)
+                    {
+                        Console.Write("O");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
